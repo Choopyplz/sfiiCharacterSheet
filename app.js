78c00cc29f809389ba54b889d1a8b48ac21e355e
Chad Cooper <chadcooper102@gmail.com>
2022-11-06 19:05:01 -0500

    final setup before plugging in database entries

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
 
 //connecting application with database
 mongoose.connect(process.env.DATABASE_URL, {
