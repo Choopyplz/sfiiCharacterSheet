e3711ea6e7c8364db4b9c8fd5134bcdb9b4adcef
Chad Cooper <chadcooper102@gmail.com>
2022-11-23 12:52:38 -0500

    page complete, need to update readme file

--- Public/scripts.js
+++ Public/scripts.js
@@ -1,11 +1,10 @@
-
-//Char Select Gallery
+//Char Select Gallery-------------------------------------
 const gallery = document.getElementById('gallery');
 const playerSelectMap = document.getElementById('header');
 
 const wMap = document.createElement('img');
-    wMap.src = 'img/wmDefault.png';
-    wMap.classList.add('worldMap');
+wMap.src = 'img/wmDefault.png';
+wMap.classList.add('worldMap');
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -20,11 +19,11 @@ iconIndexes.forEach(i => {
     icon.setAttribute('id', 'psIcon' + i);
 
     //Hover Player Select
-    icon.addEventListener('mouseover', function() {
+    icon.addEventListener('mouseover', function () {
         icon.src = `img/portraits/psHover-${i}.png`;
         wMap.src = `img/wm-${i}.png`;
     })
-    icon.addEventListener('mouseout', function() {
+    icon.addEventListener('mouseout', function () {
         icon.src = `img/portraits/ps-${i}.png`;
         wMap.src = 'img/wmDefault.png';
     })
@@ -36,17 +35,17 @@ iconIndexes.forEach(i => {
     })
 
     //Close Modal
-    window.onclick = function(e){
-        if(e.target == overlay) {
-        modal.style.display = "none"
-        overlay.style.display = 'none';
+    window.onclick = function (e) {
+        if (e.target == overlay) {
+            modal.style.display = "none"
+            overlay.style.display = 'none';
         }
     }
 
     const closeButton = document.getElementById('exit');
-    closeButton.addEventListener('click', function() {
+    closeButton.addEventListener('click', function () {
         modal.style.display = "none";
-        overlay.style.display = 'none';    
+        overlay.style.display = 'none';
     })
 
     playerSelectMap.appendChild(wMap);
@@ -54,7 +53,7 @@ iconIndexes.forEach(i => {
 
 })
 
-//Data Table
+//Data Table--------------------------------------------
 
 const url = 'http://localhost:3000/chars';
 
@@ -62,25 +61,85 @@ fetch(url)
     .then(res => {
         return res.json();
     })
-
-    .then(chars => {
+    .then(data => {
         let bioData = document.querySelector('#bioKeys');
         let out = '';
-        for(let char of chars) {
+        data.forEach(char => {
             out += `
-                <tr>
-                    <td>${char.name}</td>
-                    <td>${char.date_of_birth}</td>
-                    <td>${char.height}</td>
-                    <td>${char.weight}</td>
-                    <td>${char.blood_type}</td>
-                    <td>${char.nationality}</td>
-                    <td>${char.fighting_style}</td>
-                    <td>${char.alignment}</td>
-                </tr>
-            `;
-        }
+                    <tr>
+                        <td>${char.name}</td>
+                        <td>${char.date_of_birth}</td>
+                        <td>${char.height}</td>
+                        <td>${char.weight}</td>
+                        <td>${char.blood_type}</td>
+                        <td>${char.nationality}</td>
+                        <td>${char.fighting_style}</td>
+                        <td>${char.alignment}</td>
+                    </tr>
+                    `;
 
-        bioData.innerHTML = out;
-
+            bioData.innerHTML = out;
+        })
     })
+    .catch(error => console.log(error));
+
+
+//Chart---------------------------------------------------
+
+
+//  setup block
+let labels = ['Ryu', 'E. Honda', 'Blanka', 'Guile', 'Balrog', 'Vega', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat', 'M. Bison'];
+let colorHex = ['#ffffff', '#a8d0f7', '#f28729', '#259927', '#5684c4', '#163c70','#ed3d1a','#499bde','#bd390d','#bda30f','#374a9e','#a31219',]
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: '% Played',
+        data: [19.92, 2.26, 4.14, 13.16, 6.77, 7.52, 16.17, 8.65, 1.88, 3.01, 9.77, 6.77],
+        borderWidth: 1,
+        backgroundColor: colorHex
+    }]
+};
+
+// options block
+const options = {
+    plugins: {
+        legend: {
+            display: true,
+            labels: {
+                color: '#ffffff',
+                weight: '600',
+                textAlign: 'left',
+                font: {
+                    size: '13px',
+                    weight: 'bold'
+                }
+            }
+        },
+        title: {
+            display: true,
+            text: 'Character Popularity Based On Event Hubs Users',
+            color: '#c4e5ff',
+            font: {
+                size: '18px'
+            },
+            padding: {
+                top: 10,
+                bottom: 10
+            }
+        }
+    }
+};
+
+//  config block
+const config = {
+    type: 'pie',
+    data,
+    options
+};
+
+//  render block
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
